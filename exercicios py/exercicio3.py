2b3f379d68b8d380ee6e0e6218708adff037f23d
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-05-18 22:12:44 -0300

    testando inputs

---
+++ exercicios py/exercicio3.py
@@ -0,0 +1,11 @@
+nome = input("digite seu nome: ")
+resultado = nome
+print("seu nome e:",resultado)
+
+idade = input("diite sua idade: ")
+resultado =idade
+print("sua idade e:",resultado)
+
+altura = input("diite sua altura: ")
+resultado =altura
+print("sua altura e:",resultado)
