89de127140cc936f5bdc1b7afe1363a803602b02
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-05-19 22:42:30 -0300

    soma de dois numeros

---
+++ exercicios py/exercicio 4.py
@@ -0,0 +1,7 @@
+numero1= input("digite um numero: ")
+coleta1 = int(numero1)
+numero2= input("digite outro numero: ")
+coleta2 = int(numero2)
+
+soma = coleta1 + coleta2
+print("a soma de",coleta1,"+", coleta2,"=", soma)
