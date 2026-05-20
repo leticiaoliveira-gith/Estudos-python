9f94d77147da53941f2668449839c139e5fd4654
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-05-19 23:14:30 -0300

    soma de 3 numeros

---
+++ exercicios py/exercicio 5.py
@@ -0,0 +1,11 @@
+numero1 = input("digite um numero: ")
+coleta1 = int(numero1)
+
+numero2 = input("digite outro numero: ")
+coleta2 = int(numero2)
+
+numero3 = input("digite um numero: ")
+coleta3 = int(numero3)
+
+soma = coleta1 + coleta2 + coleta3
+print("a soma de", coleta1,"+", coleta2,"+",coleta3,"=", soma)
