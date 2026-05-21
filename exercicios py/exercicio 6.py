8221f797e06fd7ce8569cadbe003f05c29d7a1d6
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-05-20 23:11:42 -0300

    dobro e triplo de um numero

---
+++ exercicios py/exercicio 6.py
@@ -0,0 +1,11 @@
+numero1 = input("digite um numero: ")
+coleta1 = int(numero1)
+
+soma = coleta1*2
+print("o dobro de", coleta1, "é:", soma)
+
+numero2 = input("digite um numero: ")
+coleta2 = int(numero2)
+
+soma = coleta2*3
+print("o triplo de", coleta2, "é:", soma)
