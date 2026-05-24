90a4d87689b46b9c91be264a4b55498b3180c9e5
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-05-23 23:10:02 -0300

    outra forma de fazer

--- exercicios py/exercicio 4.py
+++ exercicios py/exercicio 4.py
@@ -1,7 +1,15 @@
-numero1= input("digite um numero: ")
+"""numero1= input("digite um numero: ")
 coleta1 = int(numero1)
 numero2= input("digite outro numero: ")
 coleta2 = int(numero2)
 
 soma = coleta1 / coleta2
 print("a divisão de",coleta1,"/", coleta2,"=", soma)
+ 
+outra forma de fazer sem o coleta
+"""
+numero1 = int(input("digite um numero: "))
+numero2 = int(input("digite outro numero: "))
+
+soma = numero1 + numero2
+print("a soma de",numero1,"+", numero2,"=", soma)
