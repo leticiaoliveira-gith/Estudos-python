b25dbe431584d9e8191486a2edc2d2c4d874927b
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-05-27 22:39:10 -0300

    media ponderada

---
+++ Logica_Programação/media_ponderada.py
@@ -0,0 +1,8 @@
+A = int(input("digite um numero: "))  
+PA = 2
+    
+B = int(input("digite outro numero: ")) 
+PB = 2
+media_ponderada = (A * PA + B * PB) / (PA + PB)
+    
+print("a media ponderada e: ",media_ponderada)
