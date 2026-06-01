4a77ced49298d55dfdc6057c31a3ad9e4871a0e3
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-06-01 16:26:32 -0300

    pequena mudança na estrutura

--- Logica_Programação/altura.py
+++ Logica_Programação/altura.py
@@ -1,6 +1,6 @@
 altura = float(input("qual sua altura? "))
 
-if altura < 1.65:
-    print("você e considerado uma pessoa baixa")
+if altura >= 1.65:
+    print("você e considerado uma pessoa alta")
 else:
-    print("você e alto")
+    print("você e considerado uma pessoa baixa")
