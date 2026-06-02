48afdc4c83f3ca1419b8d4d1877c3c2c2bd92a61
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-06-02 16:04:54 -0300

    impressora de boletim, e dados basicos de python

---
+++ oficina/impressora_de_boletim.py
@@ -0,0 +1,12 @@
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 17
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score,type(score))
