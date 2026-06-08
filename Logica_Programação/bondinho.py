0a23d5522b1f083f7810e38ff6023c36c42fc847
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-06-08 20:30:58 -0300

    total de pessoas que pode ter em um bondinho

---
+++ Logica_Programação/bondinho.py
@@ -0,0 +1,8 @@
+aluno = int(input("quantos alunos: "))
+monitor = int (input("quantos monitores: "))
+
+totalP = aluno + monitor
+if totalP > 50:
+    print("Muita gente n pode embarcar")
+else:
+    print("total ok liberado embarque")
